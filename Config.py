56c780bb51c6c1596658117b6d832533b493830a
AleksKhokrin <88559646+AleksKhokrin@users.noreply.github.com>
2021-09-10 21:22:43 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 TOKEN = '1956935064:AAHWVk-Z0EmkYnTugXmB5fqBaHq2OQhcgBc'
 
 keys = {
-    'евро': 'EUR',
-    'доллар': 'USD',
-    'рубль': 'RUB',
+    'Евро': 'EUR',
+    'Доллар': 'USD',
+    'Рубль': 'RUB',
 }
